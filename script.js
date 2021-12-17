7a229c74eecd290f0b3d215c22a22a2214055209
Nicholas Amemazior <amemaziornicholas77@gmail.com>
2021-12-17 22:16:39 +0000

    letting go of the project for now...

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-var input = document.getElementById("search").value;
+alert("This page/project is no longer supported by Goldac77, for now...");
+alert("You can choose to be a collaborator and continue, or create a similar one for your area/country with the templates in the code\nif this is the case, don't hesitate to get in touch");
+alert("Or simpliy fork the code, good luck!");
+var input = document.getElementById("search");
 input.addEventListener("keyup", function(event) {
-  event.preventDefault();
   if (event.keyCode === 13) {
+   event.preventDefault();
    document.getElementById("search_bt").click();
   }
 });
 
-function test() {
-  alert("Hello World!");
-}
+function selectText() {
+  var input = document.getElementById('search');
+  window.find(input.value)  //highlight desired text
+  window.getSelection().removeAllRanges();  //remover highlighted text
+};
